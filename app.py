ddb1533b315b0c49414402cb4a8597c1a0823c72
marvinganeda2 <134789171+marvinganeda2@users.noreply.github.com>
2023-05-28 00:54:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-import streamlit as st
 import tensorflow as tf
+import streamlit as st
 import cv2
 from PIL import Image, ImageOps
 import numpy as np
 
 @st.cache(allow_output_mutation=True)
-@st.cache_resource
 def load_model():
     model = tf.keras.models.load_model('cnn_model3.h5')
     return model
@@ -21,9 +20,8 @@ def import_and_predict(image_data, model):
     image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
     image = ImageOps.grayscale(image)
     img = np.asarray(image)
-    img = img.reshape((size[0], size[1]))  
-    img_reshape = img[np.newaxis, ...]
-    prediction = model.predict(img_reshape)
+    img = img.reshape((1, size[0], size[1], 1))  # Reshape to (batch_size, 28, 28, 1)
+    prediction = model.predict(img)
     return prediction
 
 
@@ -36,6 +34,7 @@ else:
     image = Image.open(file)
     st.image(image, use_column_width=True)
     prediction = import_and_predict(image, model)
+    # ...
     class_names=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 
                  'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
                  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
