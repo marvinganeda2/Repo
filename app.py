7f9dc4c6062bb7d5a0daf1b5056ec93e9100de56
marvinganeda2 <134789171+marvinganeda2@users.noreply.github.com>
2023-05-28 00:28:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import numpy as np
 @st.cache(allow_output_mutation=True)
 @st.cache_resource
 def load_model():
-    model = tf.keras.models.load_model('cnn_model3.h5')
+    model = tf.keras.models.load_model('93.h5')
     return model
 
 model = load_model()
@@ -16,17 +16,37 @@ st.write("# Braille Classification System")
 
 file = st.file_uploader("Choose braille photo from computer", type=["jpg", "png"])
 
+#def import_and_predict(image_data, model):
+    #size = (28, 28)
+    #image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    #image = ImageOps.grayscale(image)
+    #img = np.asarray(image)
+    #img = img.reshape((size[0], size[1]))  
+    #img_reshape = img[np.newaxis, ...]
+    #prediction = model.predict(img_reshape)
+    #return prediction
+
 def import_and_predict(image_data, model):
     size = (28, 28)
+    
+    # Resize the image to the expected input shape of the model
     image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
-    image = ImageOps.grayscale(image)
     img = np.asarray(image)
-    img = img.reshape((size[0], size[1]))  
-    img_reshape = img[np.newaxis, ...]
+    img = cv2.resize(img, (128, 128), interpolation=cv2.INTER_NEAREST)
+    
+    # Convert the image to grayscale if necessary
+    if img.ndim == 3 and img.shape[2] == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Reshape the image to add a channel dimension
+    img_reshape = img.reshape((1,) + img.shape + (1,))
+
+    # Make predictions using the Keras model
     prediction = model.predict(img_reshape)
     return prediction
 
-
+    
+    
 if file is None:
     st.text("Please upload an image file")
 else:
